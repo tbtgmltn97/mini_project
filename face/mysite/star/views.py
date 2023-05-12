5a87d042a0b068e9f92ecc6891c06e0a1bafb894
jinsu <jsjang96@gmail.com>
2023-05-12 17:32:54 +0900

    qwwqe

--- face/mysite/star/views.py
+++ face/mysite/star/views.py
@@ -1,8 +1,9 @@
+from .models import Post
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
-
-
+    posts = Post.objects.order_by('-views')[:5]
+    context = {'posts': posts}
+    return render(request, 'index.html', context)
