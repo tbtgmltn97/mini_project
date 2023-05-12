000e638fdd79aa35bd2e5586534107c9912f49ff
jk <user@DESKTOP-NJ8D0J6>
2023-05-12 14:27:43 +0900

    a

--- face/mysite/star/urls.py
+++ face/mysite/star/urls.py
@@ -1,11 +1,5 @@
 from django.urls import path, include
 from . import views
-<<<<<<< HEAD
 urlpatterns = [
     path('', views.index)
-=======
-
-urlpatterns = [
-    path('', views.index) 
->>>>>>> 4314a2837b96fd5699189b25de6ccbe47c2a1f10
 ]
--- face/mysite/star/views.py
+++ face/mysite/star/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-<<<<<<< HEAD
-
-# Create your views here.
-=======
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse('dkdkdkd')
->>>>>>> 4314a2837b96fd5699189b25de6ccbe47c2a1f10
